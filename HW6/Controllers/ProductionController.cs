e4b6e67b798478341b529ad965170f094ad4b9db
LeoThalman <lthalman15@wou.edu>
2017-11-12 14:52:46 -0800

    Added Dropdown menu, began productlist page

--- HW6/Controllers/ProductionController.cs
+++ HW6/Controllers/ProductionController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using HW6.Models;
+using HW6.ViewModel;
 using HW6.DAL;
 using System.Web;
 using System.Web.Mvc;
@@ -13,11 +14,19 @@ namespace HW6.Controllers
     public class ProductionController : Controller
     {
         private ProductionContext db = new ProductionContext();
+        private AdventureWorksViewModel Menu = new AdventureWorksViewModel();
         // GET: Production
         public ActionResult Index()
         {
+
+            Menu.ItemCat = db.ProductCategories.ToList();
+            Menu.ItemSubCat = db.ProductSubcategories.ToList();
             ViewBag.Title = "Home";
-            return View(db.ProductCategories.ToList());
+            return View(Menu);
+        }
+        public ActionResult ProductList()
+        {
+            return View();
         }
     }
 }
---
+++ HW6/ViewModel/AdventureWorksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HW6.Models;
+
+namespace HW6.ViewModel
+{
+    public class AdventureWorksViewModel
+    {
+        public IEnumerable<ProductCategory> ItemCat { get; set; }
+        public IEnumerable<ProductSubcategory> ItemSubCat { get; set; }
+    }
+}
